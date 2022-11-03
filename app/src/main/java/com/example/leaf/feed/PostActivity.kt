88388f151437f5d15854ee3afe2c8bc642b7a9e5
alstlr0312 <alstlr0312@naver.com>
2022-11-03 15:58:58 +0900

    feedUi

---
+++ app/src/main/java/com/example/leaf/feed/PostActivity.kt
@@ -0,0 +1,12 @@
+package com.example.leaf.feed
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.leaf.R
+
+class PostActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_post)
+    }
+}
