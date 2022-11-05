35b2be6cd71cc298659b4e0705d434e1588aa6cc
minsolkim <94881886+minsolkim@users.noreply.github.com>
2022-11-05 12:44:15 +0900

    minsol

---
+++ app/src/main/java/com/example/leaf/auth/WriteActivity.kt
@@ -0,0 +1,13 @@
+package com.example.leaf.auth
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.leaf.R
+
+
+class WriteActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_write)
+    }
+}
